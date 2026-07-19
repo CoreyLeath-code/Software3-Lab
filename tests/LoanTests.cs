c93897fb1e50735c6a6e51e2b479a549a9a9e994
Corey Leath <corey22blue@hotmail.com>
2026-07-19 15:28:10 -0400

    Add meaningful loan calculation tests

--- tests/LoanTests.cs
+++ tests/LoanTests.cs
@@ -1,15 +1,40 @@
+using LoanTracker;
 using Xunit;
 
-public class LoanTests
+public sealed class LoanTests
 {
+    private static readonly Lender TestLender = new("Test Bank", "123", "loans@example.com");
+
     [Fact]
-    public void InstallmentLoan_CalculatesPayment()
+    public void InstallmentLoan_CalculatesAmortizedPayment()
     {
-        var lender = new Lender("Test", "123", "email@test.com");
-        var loan = new InstallmentLoan(lender, "Car", 10000, 5, 3);
+        var loan = new InstallmentLoan(TestLender, "Car", 10_000m, 5m, 3);
 
-        var payment = loan.ToString();
+        Assert.Equal(299.71m, loan.MonthlyPayment);
+        Assert.Equal(loan.MonthlyPayment, loan.FinalPayment);
+    }
 
-        Assert.NotNull(payment);
+    [Fact]
+    public void InstallmentLoan_HandlesZeroInterest()
+    {
+        var loan = new InstallmentLoan(TestLender, "Equipment", 1_200m, 0m, 1);
+
+        Assert.Equal(100m, loan.MonthlyPayment);
+    }
+
+    [Fact]
+    public void BalloonLoan_ReportsInterestAndFinalPrincipalPayment()
+    {
+        var loan = new BalloonLoan(TestLender, "Property", 12_000m, 6m, 5);
+
+        Assert.Equal(60m, loan.MonthlyPayment);
+        Assert.Equal(12_060m, loan.FinalPayment);
+    }
+
+    [Fact]
+    public void Loan_RejectsNonPositivePrincipal()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => new InstallmentLoan(TestLender, "Car", 0m, 5m, 3));
     }
 }
